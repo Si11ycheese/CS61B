1c942d1c472dcf5fee4e18bf84496e52864ac669
Si11ycheese <si11ycheese6680@gmail.com>
2023-08-18 13:19:03 +0800

    Solution(By adding a method named 'isLeapYear')

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,3 +1,5 @@
+import java.nio.file.FileAlreadyExistsException;
+
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
@@ -6,6 +8,20 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    /** Use isLeapYear to tell whether the year is a leap year
+     * By use boolean
+     */
+    public static boolean isLeapYear(int year)
+    {
+        if(year%400==0||(year%4==0&&year%100!=0))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
